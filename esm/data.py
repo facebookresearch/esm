ec952e75586ae77c52f10936b18496d5faeb1430
Jason Liu <liujas000@gmail.com>
2020-09-01 23:18:00 -0700

    add shell commands to notebook and read_fasta

--- esm/data.py
+++ esm/data.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import re
 import torch
 
 
@@ -137,3 +138,39 @@ class BatchConverter(object):
             tokens[i, 1 : len(seq_str) + 1] = seq
 
         return labels, strs, tokens
+
+
+def read_fasta(
+    path, keep_gaps=True, keep_insertions=True, to_upper=False,
+):
+    with open(path, "r") as f:
+        for result in read_alignment_lines(
+            f, keep_gaps=keep_gaps, keep_insertions=keep_insertions, to_upper=to_upper
+        ):
+            yield result
+
+
+def read_alignment_lines(
+    lines, keep_gaps=True, keep_insertions=True, to_upper=False,
+):
+    seq = desc = None
+
+    def parse(s):
+        if not keep_gaps:
+            s = re.sub("-", "", s)
+        if not keep_insertions:
+            s = re.sub("[a-z]", "", s)
+        return s.upper() if to_upper else s
+
+    for line in lines:
+        # Line may be empty if seq % file_line_width == 0
+        if len(line) > 0 and line[0] == ">":
+            if seq is not None:
+                yield desc, parse(seq)
+            desc = line.strip()
+            seq = ""
+        else:
+            assert isinstance(seq, str)
+            seq += line.strip()
+    assert isinstance(seq, str) and isinstance(desc, str)
+    yield desc, parse(seq)
