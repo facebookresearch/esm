9ec571dd3ba544664e7b10fd94eb1a18e2bdc0e2
yaoyy.hi <>
2022-11-03 12:01:13 +0800

    add tools for automatically downloading/updating weights

---
+++ scripts/download_weights.sh
@@ -0,0 +1,41 @@
+#! /bin/sh
+# Usage: bash <repo>/esm/scripts/download_weights.sh /path/to/weights/
+# re run the command to continue downloading incomplete files.
+
+run_dir=$(readlink -f $(dirname $0)) ;
+
+if [[ "$(which aria2c)" == "" ]];then
+  echo aria2c is required for downloading!
+  exit 1
+fi
+
+save_dir=$1
+if [[ "$save_dir" == "" ]];then
+  save_dir=$PWD
+fi
+
+mkdir -p $save_dir
+
+pushd $save_dir
+cat $run_dir/../README.md |grep -e '^|' |grep -e 'fair-esm/models' |tr -d '|' | \
+  awk '{
+            # read urls started with https
+            split($0,arr,"https");
+            url="https"arr[2];
+            print url;
+
+            # inspect regression pt url
+            url_regression=url;
+            sub("models","regression",url_regression);
+            sub(".pt","-contact-regression.pt",url_regression);
+            print url_regression;
+
+            # download weight
+            system("if [[ ! -f $(basename "url") || -f $(basename "url").aria2 ]];then echo Download not complete: $(basename "url");aria2c -x 10 "url";else echo Download complete: $(basename "url");fi")
+
+            # download regression
+            system("if [[ ! -f $(basename "url_regression") || -f $(basename "url_regression").aria2 ]];then echo Download not complete: $(basename "url_regression");aria2c -x 10 "url_regression";else echo Download complete: $(basename "url_regression");fi")
+
+            }'
+
+popd
