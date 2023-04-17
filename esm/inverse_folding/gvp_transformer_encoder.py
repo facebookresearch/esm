f18717ba4b117aa30741bc974ad72752bf255a0c
Tom Sercu <tom.sercu@gmail.com>
2023-04-17 11:04:16 -0400

    metamorphosis

--- esm/inverse_folding/gvp_transformer_encoder.py
+++ esm/inverse_folding/gvp_transformer_encoder.py
@@ -1,4 +1,4 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
 #
 # Contents of this file were adapted from the open source fairseq repository.
 #
